c842baf124c768951e467010233262ef77e04480
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-02-03 13:48:29 +0000

    Functional CRUD back end. CSS on login page

---
+++ controller.rb
@@ -0,0 +1,35 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require('pry-byebug')
+
+require_relative( './models/employee' )
+require_relative( './models/client' )
+require_relative( './models/shift' )
+require_relative( './controllers/employee_controller.rb' )
+require_relative( './controllers/client_controller.rb' )
+require_relative( './controllers/shift_controller.rb' )
+
+
+
+get( '/healthcare' ) do 
+  @employees = Employee.all()
+  @clients = Client.all()
+  @shifts = Shift.all()
+  @healthcare = true
+  erb :login, :layout => :login_layout
+end
+
+
+get( '/home' ) do
+  @employees = Employee.all()
+  @clients = Client.all()
+  @shifts = Shift.all()
+  erb :home
+end
+
+
+
+
+
+
+
